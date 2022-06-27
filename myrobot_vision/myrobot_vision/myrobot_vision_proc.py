19a825c30fb215b57116f0c80f3957126692847e
przybylmj <przybyl@mj.com>
2022-06-27 23:49:29 +0200

    Basic disparity map optained

--- myrobot_vision/myrobot_vision/myrobot_vision_proc.py
+++ myrobot_vision/myrobot_vision/myrobot_vision_proc.py
@@ -1,9 +1,11 @@
-import encodings
+from cv2 import imwrite
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from std_srvs.srv import SetBool
 from cv_bridge import CvBridge
 import cv2 as cv
+from matplotlib import pyplot as plt
 
 class MyRobotVision(Node):
     def __init__(self):
@@ -12,26 +14,28 @@ class MyRobotVision(Node):
         self.imgLsub
         self.imgRsub = self.create_subscription(Image,'robot_model/stereocamera/right/image_raw',self.imgRcallback,10)
         self.imgRsub
-        self.brigde = CvBridge()
-        # self.saveImgs()
+        self.br = CvBridge()
+        self.create_service(SetBool,"stereo_count",self.stereoCount)
 
     def imgLcallback(self,msg):
-        imgL = self.brigde.imgmsg_to_cv2(msg,desired_encoding= 'bgra8')
-        cv.imshow("CameraL",imgL)
-        cv.waitKey(0)
-
-    def imgRcallback(self,msg):
-        pass
-        # imgR = self.brigde.imgmsg_to_cv2(msg,desired_encoding='bgra8')
-        # cv.imshow("CameraR",imgR)
-        # cv.waitKey(0)
+        self.imgL = self.br.imgmsg_to_cv2(msg,desired_encoding = 'bgra8')
         
-    
-    def saveImgs(self):
-        imgL = self.brigde.imgmsg_to_cv2(self.imgLmsg,desired_encoding='passthrough')
-        imgR = self.brigde.imgmsg_to_cv2(self.imgRmsg,desired_encoding='passthrough')
-        cv.imwrite('/home/jan/ws_myrobot/src/myrobot_vision/dnn/imgL.jpg',imgL)
-        cv.imwrite('/home/jan/ws_myrobot/src/myrobot_vision/dnn/imgR.jpg',imgR)
+    def imgRcallback(self,msg):
+        self.imgR = self.br.imgmsg_to_cv2(msg,desired_encoding = 'bgra8')
+
+    def stereoCount(self,request,response):
+        self.get_logger().info("Stereo method called")
+        # cv.imwrite("/home/jan/ws_myrobot/src/myrobot_vision/dnn/imgL.png",self.imgL)
+        # cv,imwrite("/home/jan/ws_myrobot/src/myrobot_vision/dnn/imgR.png",self.imgR)
+        # cv.imshow("CameraRGray",cv.cvtColor(self.imgR,cv.COLOR_BGRA2GRAY))
+        # cv.waitKey(0)
+        stereo = cv.StereoBM_create(numDisparities=16,blockSize=15)
+        disparity = stereo.compute(cv.cvtColor(self.imgR,cv.COLOR_BGRA2GRAY),cv.cvtColor(self.imgL,cv.COLOR_BGRA2GRAY))
+        plt.imshow(disparity,'gray')
+        plt.show()
+        response.success = True
+        return response
+        
 
 def main(args=None):
     rclpy.init(args=args)
